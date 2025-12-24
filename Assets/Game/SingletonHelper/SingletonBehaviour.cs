e5d955ead3ee8c0e503bb4eccc932c9d0d7c77f6
Imperious0 <alparslan226@gmail.com>
2025-12-24 22:56:52 +0300

    Update: SingletonBehaviour

--- Assets/Game/SingletonHelper/SingletonBehaviour.cs
+++ Assets/Game/SingletonHelper/SingletonBehaviour.cs
@@ -2,10 +2,29 @@ using UnityEngine;
 
 namespace Game.SingletonHelper
 {
-    public abstract class SingletonBehaviour<T> : MonoBehaviour where T : class
+    public abstract class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
     {
         private static T _instance;
-        public static T Instance { get => _instance; }
+        public static T Instance {
+            get
+            {
+                if (!_instance)
+                {
+                    _instance = (T)FindFirstObjectByType(typeof(T));
+                    if (!_instance)
+                    {
+                        Debug.LogWarning($"Cant find {typeof(T)} in Scene");
+                    }
+                    else
+                    {
+                        SingletonBehaviour<T> instance = _instance as SingletonBehaviour<T>;
+                        instance.OnAwake();
+                    }
+                }
+
+                return _instance;
+            }
+        }
 
         protected void Awake()
         {
